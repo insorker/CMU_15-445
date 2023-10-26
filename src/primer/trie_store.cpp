db32ea50c89caddbd93e1ab1f4f7c4c1a8ec0f38
insorker <2677331029@qq.com>
2023-10-26 20:01:05 +0800

    UPDATE: P0T2

--- src/primer/trie_store.cpp
+++ src/primer/trie_store.cpp
@@ -5,27 +5,63 @@ namespace bustub {
 
 template <class T>
 auto TrieStore::Get(std::string_view key) -> std::optional<ValueGuard<T>> {
-  
   // Pseudo-code:
   // (1) Take the root lock, get the root, and release the root lock. Don't lookup the value in the
   //     trie while holding the root lock.
+  this->root_lock_.lock();
+  Trie root = this->root_;
+  this->root_lock_.unlock();
+
+  // Pseudo-code:
   // (2) Lookup the value in the trie.
+  const T *value = root.Get<T>(key);
+
+  // Pseudo-code:
   // (3) If the value is found, return a ValueGuard object that holds a reference to the value and the
   //     root. Otherwise, return std::nullopt.
-  throw NotImplementedException("TrieStore::Get is not implemented.");
+  if (value == nullptr) {
+    return std::nullopt;
+  }
+  return std::optional<ValueGuard<T>>(ValueGuard(root, *value));
+
+  // throw NotImplementedException("TrieStore::Get is not implemented.");
 }
 
 template <class T>
 void TrieStore::Put(std::string_view key, T value) {
+  this->write_lock_.lock();
+  this->root_lock_.lock();
+  auto root = this->root_;
+  this->root_lock_.unlock();
+
+  root = root_.Put<T>(key, std::move(value));
+
+  this->root_lock_.lock();
+  this->root_ = root;
+  this->root_lock_.unlock();
+  this->write_lock_.unlock();
+
   // You will need to ensure there is only one writer at a time. Think of how you can achieve this.
   // The logic should be somehow similar to `TrieStore::Get`.
-  throw NotImplementedException("TrieStore::Put is not implemented.");
+  // throw NotImplementedException("TrieStore::Put is not implemented.");
 }
 
 void TrieStore::Remove(std::string_view key) {
+  this->write_lock_.lock();
+  this->root_lock_.lock();
+  auto root = this->root_;
+  this->root_lock_.unlock();
+
+  root = root_.Remove(key);
+
+  this->root_lock_.lock();
+  this->root_ = root;
+  this->root_lock_.unlock();
+  this->write_lock_.unlock();
+
   // You will need to ensure there is only one writer at a time. Think of how you can achieve this.
   // The logic should be somehow similar to `TrieStore::Get`.
-  throw NotImplementedException("TrieStore::Remove is not implemented.");
+  // throw NotImplementedException("TrieStore::Remove is not implemented.");
 }
 
 // Below are explicit instantiation of template functions.
